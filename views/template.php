ed736211ac162f5e029b945de485950c51cf9cc9
Tiago <tiagosantosf6@gmail.com>
2025-04-24 22:17:25 -0300

    create/archives MVC

---
+++ views/template.php
@@ -0,0 +1,22 @@
+<!-- Toda a parte fixa do site está aqui neste arquivo -->
+ <!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Site Institucional</title>
+    <!-- <link rel="stylesheet" type="text/css" href="assets/css/template.css"/> -->
+    <link rel="stylesheet" href="/assets/css/template.css" type="text/css" />
+</head>
+
+<body>
+    <div class="topo">
+        <div class="topo1"></div>
+        <div class="banner"></div>
+    </div>
+    <div class="menu">
+    </div>
+    <div class="container">
+    </div>
+    <div class="rodape"></div>
+</body>
+
+</html>
